354a65f1d44ed56cb250ceac0dddf076112dafb5
bokdong <altmen77@naver.com>
2024-08-09 15:12:18 +0900

    08091512 김현우 - DB 연결 로그인 추가

---
+++ app/src/main/java/com/example/groupware/connectDB/UserLoginRequest.kt
@@ -0,0 +1,25 @@
+package com.example.groupware.connectDB
+
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.Response
+import com.example.groupware.loginScreen.UserInfo
+
+class UserLoginRequest(
+    userInfo: UserInfo,
+    listener: Response.Listener<String>,
+    errorListener: Response.ErrorListener
+) : StringRequest(Method.POST, URL, listener, errorListener) {
+
+    private val parameters: Map<String, String> = mapOf(
+        "userID" to userInfo.email,
+        "password" to userInfo.password,
+    )
+
+    override fun getParams(): Map<String, String> {
+        return parameters
+    }
+
+    companion object {
+        private const val URL = "http://192.168.45.237/query/signup.php"
+    }
+}
