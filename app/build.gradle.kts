b7e663a38c145720b601136c55be8a2457f3b1cf
bokdong <altmen77@naver.com>
2024-08-13 10:43:21 +0900

    08131043 김현우 - 푸시

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,4 +96,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation ("com.google.code.gson:gson:2.10.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 }
